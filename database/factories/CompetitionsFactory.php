29f17c622456320f7b8f6663578a84016dd7f409
tri-white <olegovic101@gmail.com>
2024-01-19 09:04:56 +0200

    added competitionsFactory

---
+++ database/factories/CompetitionsFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Competitions>
+ */
+class CompetitionsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'=>fake()->word(),
+            'event_date'=>fake()->dateTime(),
+            'event_location'=>fake()->address(),
+            'prize_pool'=>fake()->numberBetween(10000,1000000),
+            'sports_type'=>fake()->randomElement([
+                '100m sprint',
+                '3km run',
+                'spear throwing',
+                'football',
+                'tennis',
+            ]),
+        ];
+    }
+}
