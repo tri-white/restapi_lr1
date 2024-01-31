699efa0598617f166723d573a566b8da96c57465
tri-white <olegovic101@gmail.com>
2024-01-31 09:27:57 +0200

    added first component and used it

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Header from './Header';
+import axios from "axios";
 function Example() {
+    // axios.post('route url', {data});
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
+            <>
+                        <Header />
 
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
+            </>
+
     );
 }
 
